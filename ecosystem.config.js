c67030a3542fc728bc427046e82146e01538da03
ToanVQ <hunter9xbn@gmail.com>
2019-12-26 22:20:44 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	apps: [{
+		name: 'TrainingAPI',
+		script: 'index.js',
+		env: {
+			NODE_ENV: 'development',
+		},
+		env_production: {
+			NODE_ENV: 'production',
+		},
+	}],
+
+	deploy: {
+		production: {
+			user: 'toanvq',
+			key: '/Users/toanvq/Documents/Keys/ubuntu@my-aws-ec2.pem',
+			host: '52.77.254.77',
+			ref: 'origin/master',
+			repo: 'https://github.com/cr4zyc4t/react-training-server.git',
+			path: '/home/ubuntu/training-api-server',
+			'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+		},
+	},
+};
