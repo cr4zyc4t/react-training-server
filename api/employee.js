f6cb9b32a8129961b113414cc264999ca881bb87
ToanVQ <hunter9xbn@gmail.com>
2019-12-31 14:47:09 +0700

    delay

--- api/employee.js
+++ api/employee.js
@@ -3,16 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.json([
-		{
-			id: 1,
-			employee_name: "Mrs. Janessa Hackett",
-		},
-		{
-			id: 2,
-			employee_name: "Dr. Orie Ernser",
-		}
-	])
+	setTimeout(() => {
+		res.json([
+			{
+				id: 1,
+				employee_name: "Mrs. Janessa Hackett",
+			},
+			{
+				id: 2,
+				employee_name: "Dr. Orie Ernser",
+			}
+		])
+	}, 2000);
 });
 
 module.exports = router;
